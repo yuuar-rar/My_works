0d44afb34b95672b494e9c20bd7f32dd55835c43
yuuar-rar <72976361+yuuar-rar@users.noreply.github.com>
2024-10-29 11:55:50 +1000

    Изменена прокрутка картинок в Мои работы(сайты)

---
+++ slider_script.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.slider_One');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('.slider-item'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  slide();
+});
+
+nextButton.addEventListener('click', () => {
+  slideIndex = (slideIndex + 1) % slideCount;
+  slide();
+});
+
+const slide = () => {
+  const imageWidth = slider.clientWidth;
+  const slideOffset = -slideIndex * imageWidth;
+  slider.style.transform = `translateX(${slideOffset}px)`;
+}
+
+window.addEventListener('load', () => {
+  slide();
+});
